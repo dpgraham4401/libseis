aa61666d9d2a747b703b953338319cab54462fab
David Graham <dpgraham4401@gmail.com>
2022-12-03 21:28:34 -0500

    convert all current work to double as default

--- test/libseis_test.cc
+++ test/libseis_test.cc
@@ -7,49 +7,49 @@ extern "C" {
 int const nt_test = 10;
 int const nx_test = 1;
 
-float float_trace1[nt_test] = {
-    0.000,
-    2.000,
-    4.000,
-    8.000,
-    1.000,
-    2.000,
-    3.000,
-    4.000,
-    5.000,
-    6.000,
+double double_trace1[nt_test] = {
+        0.000,
+        2.000,
+        4.000,
+        8.000,
+        1.000,
+        2.000,
+        3.000,
+        4.000,
+        5.000,
+        6.000,
 };
 
-float cmp_array[nt_test * 2] = {
-    0.000,
-    2.000,
-    4.000,
-    8.000,
-    1.000,
-    2.000,
-    3.000,
-    4.000,
-    5.000,
-    6.000,
-    0.000,
-    0.000,
-    1.000,
-    0.020,
-    0.301,
-    -0.101,
-    0.035,
-    0.011,
-    -0.006,
-    0.009,
+double cmp_array[nt_test * 2] = {
+        0.000,
+        2.000,
+        4.000,
+        8.000,
+        1.000,
+        2.000,
+        3.000,
+        4.000,
+        5.000,
+        6.000,
+        0.000,
+        0.000,
+        1.000,
+        0.020,
+        0.301,
+        -0.101,
+        0.035,
+        0.011,
+        -0.006,
+        0.009,
 };
 
 struct Gather cmp = {
-    1,
-    nt_test,
-    2,
-    1,
-    cmp_array,
-    DOUBLE
+        1,
+        nt_test,
+        2,
+        1,
+        cmp_array,
+        DOUBLE
 };
 
 TEST(HelloTest, ExampleTest) {
@@ -58,15 +58,15 @@ TEST(HelloTest, ExampleTest) {
 
 TEST(LibseisIO, ReadWriteFloat) {
     char tmp_file[] = "/tmp/data.bin";
-    write_float(tmp_file, float_trace1, nt_test, nx_test);
-    float *data = read_float(tmp_file, nt_test, nx_test);
+    write_double(tmp_file, double_trace1, nt_test, nx_test);
+    double *data = read_double(tmp_file, nt_test, nx_test);
     for (int i = 0; i < nt_test; i++) {
-        EXPECT_EQ(data[i], float_trace1[i]);
+        EXPECT_EQ(data[i], double_trace1[i]);
     }
 }
 
 TEST(GainCmp, GainsCmpGather) {
-    float t_pow = 2;
+    double t_pow = 2;
     Gather *foo = gain_gather(&cmp, t_pow);
     printf("%d\n", foo->nt);
     EXPECT_EQ(0, 0);

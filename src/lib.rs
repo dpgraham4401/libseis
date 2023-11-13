05e43691cf23db9e7aa00b4c78064ab461bc90f8
David Paul Graham <dpgraham4401@gmail.com>
2023-11-12 22:22:16 -0500

    saving work

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,29 @@
+use std::io::Read;
 
-#[derive()]
+#[derive(Debug)]
 pub struct Gather {
     id: u16,
-    number_time_samples: u16,
-    number_traces: u16,
-    time_sampling: f32,
-    data: Vec<f32>,
+    nt: u16,
+    nx: u16,
+    dt: f32,
+    data: Vec<u8>,
+}
+
+fn read_binary_file(file_name: &str) -> Vec<u8> {
+    let mut file = std::fs::File::open(file_name).unwrap();
+    let mut buffer = Vec::new();
+    file.read_to_end(&mut buffer).unwrap();
+    buffer
+}
+
+fn load_data_into_gather(path: &str, time_sampling: f32, samples_per_trace: u16, number_of_traces: u16 ) -> Gather {
+    Gather {
+        id: 1,
+        data: read_binary_file(path),
+        dt: time_sampling,
+        nt: samples_per_trace,
+        nx: number_of_traces,
+    }
 }
 
 #[cfg(test)]
@@ -15,5 +33,12 @@ mod tests {
     #[test]
     fn it_works() {
         assert_eq!(2, 2);
+    }
+
+    #[test]
+    fn reads_binary_file() {
+        let file_path: &str = "./resources/shot6220.bin";
+        let gather = load_data_into_gather(file_path, 0.004, 3101, 1000);
+        println!("Gather: {:?}", gather)
     }
 }

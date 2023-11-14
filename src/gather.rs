4b5d1862fb6136d24cabe2a36ac9c597cebbefdd
David Graham <dpgraham4401@gmail.com>
2023-11-14 12:56:36 -0500

    add error handling for read_binary_file

--- src/gather.rs
+++ src/gather.rs
@@ -1,3 +1,4 @@
+use std::fs;
 use std::io::Read;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -12,27 +13,18 @@ pub struct Gather<T> {
     pub nt: u16,
     pub nx: u16,
     pub dt: f32,
-    pub data: Option<Vec<T>>,
+    pub data: Option<Box<T>>,
     precision: Option<Precision>,
 }
 
-fn read_binary_file(file_name: &str) -> Vec<u8> {
-    let mut file = std::fs::File::open(file_name).unwrap();
-    let mut buffer = Vec::new();
-    file.read_to_end(&mut buffer).unwrap();
-    buffer
-}
-
-fn load_data_into_gather<T>(path: &str, time_sampling: f32, samples_per_trace: u16, number_of_traces: u16 ) -> Gather<T> {
-    let data_from_file = read_binary_file(path);
-    Gather {
-        id: 1,
-        dt: time_sampling,
-        nt: samples_per_trace,
-        nx: number_of_traces,
-        precision: None,
-        data: None,
-    }
+#[allow(dead_code)]
+pub fn read_binary_file(file_name: &str) -> Result<Vec<u8>, String>{
+    let raw_data = fs::read(file_name);
+    let data = match raw_data {
+        Ok(data) => data,
+        Err(e) => return Err(format!("Error reading file: {}", e)),
+    };
+    Ok(data)
 }
 
 #[cfg(test)]
@@ -40,14 +32,9 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        assert_eq!(2, 2);
-    }
-
-    #[test]
     fn reads_binary_file() {
         let file_path: &str = "./resources/shot6220.bin";
-        let gather = load_data_into_gather(file_path, 0.004, 3101, 1000);
-        println!("Gather: {:?}", gather)
+        let raw_data = read_binary_file(file_path);
+        println!("Gather: {:?}", raw_data)
     }
 }

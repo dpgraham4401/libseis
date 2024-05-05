c8bb196743a0e1bdc51604d186fcd3717301794d
David Paul Graham <43794491+dpgraham4401@users.noreply.github.com>
2024-05-05 00:07:30 -0400

    Velocity models (#6)
    * add velocity model object and allocate_velocity_model utility function
    
    * .cc file to .cpp
    
    * new velocity testing file
    
    * set up separate files for velocity and libseis
    
    * simple velocity model function for generating quick velocity model with just a starting velocity and gradient

--- src/libseis.h
+++ src/libseis.h
@@ -3,21 +3,23 @@
 // David Graham - 2023-15-01
 
 typedef enum {
-    SINGLE,
-    DOUBLE,
+  SINGLE,
+  DOUBLE,
 } PRECISION;
 
 /**
  * @brief A collection of seismic traces and attributes
  *
- * @details A gather holds encapsulates all the information about seismic gathers,
- * including the number of traces, the time sampling, number of time samples, and the data itself.
- * It's intended to be generic enough to be used by various seismic gather types, such as CMP, or shot.
+ * @details A gather holds encapsulates all the information about seismic
+ * gathers, including the number of traces, the time sampling, number of time
+ * samples, and the data itself. It's intended to be generic enough to be used
+ * by various seismic gather types, such as CMP, or shot.
  *
  * @param
  * id: (int) The gather's unique identifier
  * @param
- * nt: (int) number times samples in each trace, assumed homogeneous number of samples in each trace
+ * nt: (int) number times samples in each trace, assumed homogeneous number of
+ * samples in each trace
  * @param
  * nx: (int) number traces
  * @param
@@ -25,15 +27,16 @@ typedef enum {
  * @param
  * data: (pointer) pointer to the array of samples
  * @param
- * precision: (enum PRECISION) the precision of the samples (e.g., double, double)
+ * precision: (enum PRECISION) the precision of the samples (e.g., double,
+ * double)
  */
 typedef struct Gather {
-    int id;
-    int nt;
-    int nx;
-    double dt;
-    double *data;
-    PRECISION precision;
+  int id;
+  int nt;
+  int nx;
+  double dt;
+  double *data;
+  PRECISION precision;
 } Gather;
 
 double *read_double(char path[], int nt, int nx);
@@ -44,19 +47,21 @@ void write_double(char path[], double *data, int nt, int nx);
  * gain_gather
  *
  * @details
- * Applies a time-variant scaling to a series of seismic traces in the same gather.
- * It can be used to compensate for exponential decay (constant Q) in amplitude with time
- * See "Tpow: an estimator of seismic amplitude decay" by Fowler & Claerbout
- * nt' = nt * (dt * t^pow)
+ * Applies a time-variant scaling to a series of seismic traces in the same
+ * gather. It can be used to compensate for exponential decay (constant Q) in
+ * amplitude with time See "Tpow: an estimator of seismic amplitude decay" by
+ * Fowler & Claerbout nt' = nt * (dt * t^pow)
  *
  * @param
- * gather: A pointer to a shot, CMP, or other seismic gather with all necessary fields.
+ * gather: A pointer to a shot, CMP, or other seismic gather with all necessary
+ * fields.
  * @param
  * power: The power (double) applied to the seismic trace as a function of time.
  *
  * @returns
  * A pointer to a new gather with the supplied Tpow applied.
- * All gather attributes, besides the seismic data, are copied from the original gather
+ * All gather attributes, besides the seismic data, are copied from the original
+ * gather
  *
  */
 Gather *gain_gather(Gather *gather, double power);
---
+++ test/libseis_test.cpp
@@ -0,0 +1,44 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "libseis.h"
+#include <math.h>
+}
+
+int const nt_test = 10;
+int const nx_test = 1;
+
+double double_trace1[nt_test] = {
+    0.000, 2.000, 4.000, 8.000, 1.000, 2.000, 3.000, 4.000, 5.000, 6.000,
+};
+
+double cmp_array[nt_test * 2] = {
+    0.000, 2.000, 4.000, 8.000, 1.000, 2.000,  3.000, 4.000, 5.000,  6.000,
+    0.000, 0.000, 1.000, 0.020, 0.301, -0.101, 0.035, 0.011, -0.006, 0.009,
+};
+
+struct Gather cmp = {1, nt_test, 2, 1, cmp_array, DOUBLE};
+
+TEST(LibseisIO, ReadWriteFloat) {
+  char tmp_file[] = "/tmp/data.bin";
+  write_double(tmp_file, double_trace1, nt_test, nx_test);
+  double *data = read_double(tmp_file, nt_test, nx_test);
+  for (int i = 0; i < nt_test; i++) {
+    EXPECT_EQ(data[i], double_trace1[i]);
+  }
+}
+
+TEST(GainCmp, GainsCmpGather) {
+  double t_pow = 2;
+  Gather *foo = gain_gather(&cmp, t_pow);
+
+  printf("Check that the t^power function operates correctly");
+  for (int i = 0; i < foo->nt; i++) {
+    printf("%f * (%f^%f) = %f\n", cmp.data[i], foo->dt * i, t_pow,
+           foo->data[i]);
+    double t_powered = cmp.data[i] * pow(foo->dt * i, t_pow);
+    EXPECT_EQ(foo->data[i], t_powered);
+  }
+}
+
+TEST(Gather, GatherDisplay) { display_gather(&cmp); }

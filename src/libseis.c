6d143e85886eaa2ab89029286257f6dfaa004a0d
David Graham <dpgraham4401@gmail.com>
2024-05-04 22:16:43 -0400

    add velocity model object and allocate_velocity_model utility function

--- src/libseis.c
+++ src/libseis.c
@@ -1,75 +1,90 @@
 #include "libseis.h"
+#include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <math.h>
 
 double *read_double(char path[], int nt, int nx) {
-    FILE *fptr;
-    double *data;
-    data = (double *) malloc(nt * nx * sizeof(double));
-    fptr = fopen(path, "rb");
-    if (fptr == NULL) {
-        printf("unable to open file at %s\n", path);
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < nt * nx; i++) {
-        fread(&data[i], sizeof(double), 1, fptr);
-    }
-    fclose(fptr);
-    return data;
+  FILE *fptr;
+  double *data;
+  data = (double *)malloc(nt * nx * sizeof(double));
+  fptr = fopen(path, "rb");
+  if (fptr == NULL) {
+    printf("unable to open file at %s\n", path);
+    exit(EXIT_FAILURE);
+  }
+  for (int i = 0; i < nt * nx; i++) {
+    fread(&data[i], sizeof(double), 1, fptr);
+  }
+  fclose(fptr);
+  return data;
 }
 
 void write_double(char path[], double *data, int nt, int nx) {
-    FILE *fptr;
-    fptr = fopen(path, "wb");
-    if (fptr == NULL) {
-        printf("unable to open file at %s\n", path);
-        exit(EXIT_FAILURE);
-    }
-    for (int i = 0; i < nx * nt; i++) {
-        fwrite(&data[i], sizeof(double), 1, fptr);
-    }
-    fclose(fptr);
+  FILE *fptr;
+  fptr = fopen(path, "wb");
+  if (fptr == NULL) {
+    printf("unable to open file at %s\n", path);
+    exit(EXIT_FAILURE);
+  }
+  for (int i = 0; i < nx * nt; i++) {
+    fwrite(&data[i], sizeof(double), 1, fptr);
+  }
+  fclose(fptr);
 }
 
 /**
- * Creates a new gather with the same data as the input gather, but with each sample
- * multiplied by a power of its corresponding time value.
+ * Creates a new gather with the same data as the input gather, but with each
+ * sample multiplied by a power of its corresponding time value.
  *
  * @param gather The input gather to be multiplied.
  * @param power The power to raise the time values to.
- * @return A new gather with the same dimensions as the input gather, but with modified data.
+ * @return A new gather with the same dimensions as the input gather, but with
+ * modified data.
  */
 Gather *gain_gather(Gather *gather, double power) {
-    Gather *new_gather = malloc(sizeof(Gather));
-    if (new_gather == NULL) {
-        fprintf(stderr, "Error: input gather is NULL\n");
+  Gather *new_gather = malloc(sizeof(Gather));
+  if (new_gather == NULL) {
+    fprintf(stderr, "Error: input gather is NULL\n");
+  }
+  memcpy(new_gather, gather, sizeof(Gather));
+  new_gather->data = malloc(gather->nt * gather->nx * sizeof(double));
+  if (new_gather->data == NULL) {
+    fprintf(stderr, "Error: input gather data is NULL\n");
+  }
+  memcpy(new_gather->data, gather->data,
+         gather->nt * gather->nx * sizeof(double));
+  for (int trace_index = 0; trace_index < gather->nx; trace_index++) {
+    for (int sample_index = 0; sample_index < gather->nt; sample_index++) {
+      double time = (double)sample_index * new_gather->dt;
+      double t_pow = pow(time, power);
+      int data_index = (trace_index * new_gather->nt) + sample_index;
+      new_gather->data[data_index] = gather->data[data_index] * t_pow;
     }
-    memcpy(new_gather, gather, sizeof(Gather));
-    new_gather->data = malloc(gather->nt * gather->nx * sizeof(double));
-    if (new_gather->data == NULL) {
-        fprintf(stderr, "Error: input gather data is NULL\n");
-    }
-    memcpy(new_gather->data, gather->data, gather->nt * gather->nx * sizeof(double));
-    for (int trace_index = 0; trace_index < gather->nx; trace_index++) {
-        for (int sample_index = 0; sample_index < gather->nt; sample_index++) {
-            double time = (double) sample_index * new_gather->dt;
-            double t_pow = pow(time, power);
-            int data_index = (trace_index * new_gather->nt) + sample_index;
-            new_gather->data[data_index] = gather->data[data_index] * t_pow;
-        }
-    }
-    return new_gather;
+  }
+  return new_gather;
 }
 
 void display_gather(Gather *gather) {
-    printf("--------------------\n");
-    printf("Gather: id %d\n", gather->id);
-    printf("Sample Rate: %f seconds\n", gather->dt);
-    printf("Sampling Frequency: %f Hz\n", 1 / gather->dt);
-    printf("Nyquist Frequency: %f Hz\n", 1 / (gather->dt * 2));
-    printf("Samples per trace: %d\n", gather->nt);
-    printf("Number of traces: %d\n", gather->nx);
-    printf("--------------------\n");
+  printf("--------------------\n");
+  printf("Gather: id %d\n", gather->id);
+  printf("Sample Rate: %f seconds\n", gather->dt);
+  printf("Sampling Frequency: %f Hz\n", 1 / gather->dt);
+  printf("Nyquist Frequency: %f Hz\n", 1 / (gather->dt * 2));
+  printf("Samples per trace: %d\n", gather->nt);
+  printf("Number of traces: %d\n", gather->nx);
+  printf("--------------------\n");
+}
+
+VelocityModel *allocate_velocity_model(const int rows, const int cols) {
+  VelocityModel *model = malloc(sizeof(VelocityModel));
+  model->rows = rows;
+  model->cols = cols;
+
+  model->data = (double **)malloc(rows * sizeof(double *));
+  for (int i = 0; i < rows; i++) {
+    model->data[i] = (double *)malloc(cols * sizeof(double));
+  }
+
+  return model;
 }
--- test/libseis_test.cc
+++ test/libseis_test.cc
@@ -1,79 +1,74 @@
 #include "gtest/gtest.h"
 
 extern "C" {
-#include <math.h>
 #include "libseis.h"
+#include <math.h>
 }
 
 int const nt_test = 10;
 int const nx_test = 1;
 
 double double_trace1[nt_test] = {
-        0.000,
-        2.000,
-        4.000,
-        8.000,
-        1.000,
-        2.000,
-        3.000,
-        4.000,
-        5.000,
-        6.000,
+    0.000, 2.000, 4.000, 8.000, 1.000, 2.000, 3.000, 4.000, 5.000, 6.000,
 };
 
 double cmp_array[nt_test * 2] = {
-        0.000,
-        2.000,
-        4.000,
-        8.000,
-        1.000,
-        2.000,
-        3.000,
-        4.000,
-        5.000,
-        6.000,
-        0.000,
-        0.000,
-        1.000,
-        0.020,
-        0.301,
-        -0.101,
-        0.035,
-        0.011,
-        -0.006,
-        0.009,
+    0.000, 2.000, 4.000, 8.000, 1.000, 2.000,  3.000, 4.000, 5.000,  6.000,
+    0.000, 0.000, 1.000, 0.020, 0.301, -0.101, 0.035, 0.011, -0.006, 0.009,
 };
 
-struct Gather cmp = {
-        1,
-        nt_test,
-        2,
-        1,
-        cmp_array,
-        DOUBLE
-};
+struct Gather cmp = {1, nt_test, 2, 1, cmp_array, DOUBLE};
 
 TEST(LibseisIO, ReadWriteFloat) {
-    char tmp_file[] = "/tmp/data.bin";
-    write_double(tmp_file, double_trace1, nt_test, nx_test);
-    double *data = read_double(tmp_file, nt_test, nx_test);
-    for (int i = 0; i < nt_test; i++) {
-        EXPECT_EQ(data[i], double_trace1[i]);
-    }
+  char tmp_file[] = "/tmp/data.bin";
+  write_double(tmp_file, double_trace1, nt_test, nx_test);
+  double *data = read_double(tmp_file, nt_test, nx_test);
+  for (int i = 0; i < nt_test; i++) {
+    EXPECT_EQ(data[i], double_trace1[i]);
+  }
 }
 
 TEST(GainCmp, GainsCmpGather) {
-    double t_pow = 2;
-    Gather *foo = gain_gather(&cmp, t_pow);
+  double t_pow = 2;
+  Gather *foo = gain_gather(&cmp, t_pow);
 
-    printf("Check that the t^power function operates correctly");
-    for (int i = 0; i < foo->nt; i++) {
-        printf("%f * (%f^%f) = %f\n", cmp.data[i], foo->dt * i, t_pow, foo->data[i]);
-        double t_powered = cmp.data[i] * pow(foo->dt * i, t_pow);
-        EXPECT_EQ(foo->data[i], t_powered);
-    }
+  printf("Check that the t^power function operates correctly");
+  for (int i = 0; i < foo->nt; i++) {
+    printf("%f * (%f^%f) = %f\n", cmp.data[i], foo->dt * i, t_pow,
+           foo->data[i]);
+    double t_powered = cmp.data[i] * pow(foo->dt * i, t_pow);
+    EXPECT_EQ(foo->data[i], t_powered);
+  }
 }
 
-TEST(Gather, GatherDisplay) {
-    display_gather(&cmp);
+TEST(Gather, GatherDisplay) { display_gather(&cmp); }
+
+TEST(VelocityModelTest, AllocateVelocityModel) {
+  int rows = 5;
+  int cols = 5;
+
+  // Call the function to test
+  VelocityModel *model = allocate_velocity_model(rows, cols);
+
+  // Check if the function returned a non-null pointer
+  ASSERT_NE(model, nullptr);
+
+  // Check if the rows and cols are correctly set
+  EXPECT_EQ(model->rows, rows);
+  EXPECT_EQ(model->cols, cols);
+
+  // Check if the data array is correctly allocated
+  ASSERT_NE(model->data, nullptr);
+
+  // Check each row of the data array
+  for (int i = 0; i < rows; i++) {
+    ASSERT_NE(model->data[i], nullptr);
+  }
+
+  // Free the allocated memory
+  for (int i = 0; i < rows; i++) {
+    free(model->data[i]);
+  }
+  free(model->data);
+  free(model);
 }

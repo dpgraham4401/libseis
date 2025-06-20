3c7ad1bff6bcc470144e1b7edb262e6e486aa6e4
David Graham <dpgraham4401@gmail.com>
2025-06-19 20:52:08 -0400

    expand seismic gather unit tests

--- tests/libseis/test_gather.py
+++ tests/libseis/test_gather.py
@@ -33,8 +33,26 @@ class TestSeismicGather:
 class TestReadSeismicData:
     """Test reading seismic data."""
 
-    def test_read_gather(self, seismic_trace_1d_file):
+    @pytest.fixture
+    def nt(self) -> int:
+        """Number of time samples."""
+        return 4
+
+    @pytest.fixture
+    def nx(self) -> int:
+        """Number of spatial samples."""
+        return 1
+
+    def test_read_gather(self, seismic_trace_1d_file, nt, nx):
         """Test reading a gather."""
         # Assuming the Gather class has a method to read data
-        gather = Gather.from_bin_file(seismic_trace_1d_file, 4, 1, 0.004)
+        gather = Gather.from_bin_file(seismic_trace_1d_file, nt, nx, 0.004)
         assert isinstance(gather.data, np.ndarray)
+
+    def test_loads_dimensions_correctly(self, seismic_trace_1d_file, nt, nx):
+        """Test reading a gather."""
+        # Assuming the Gather class has a method to read data
+        NUM_DIM = 2
+        gather = Gather.from_bin_file(seismic_trace_1d_file, nt, nx, 0.004)
+        assert gather.data.ndim == NUM_DIM
+        assert gather.data.size == nt * nx
